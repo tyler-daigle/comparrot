1f497d7dbcc5f260a9018e0e42b9656a52b15525
Tyler D <tyler@tdaigle.com>
2023-08-12 15:31:15 -0400

    temp slider working

--- src/components/Comparrot.tsx
+++ src/components/Comparrot.tsx
@@ -1,5 +1,6 @@
 import ComparrotContainer from "./ComparrotContainer";
 import ComparrotImage from "./ComparrotImage";
+import { useState } from "react";
 
 interface Props {
   leftImage: string;
@@ -9,10 +10,32 @@ interface Props {
 
 
 export default function Comparrot({ leftImage, rightImage }: Props) {
+  const [leftWidth, setLeftWidth] = useState(50);
+  const [rightWidth, setRightWidth] = useState(50);
+
+  const adjustLeft = (val: number) => {
+    // the two values add up to 100%, so subtract the value we are setting
+    // from 100 to get the value of the other side
+    const right = 100 - val;
+    setLeftWidth(val);
+    setRightWidth(right);
+  };
+
+  const adjustRight = (val: number) => {
+    const left = 100 - val;
+    setRightWidth(val);
+    setLeftWidth(left);
+  };
+
   return (
-    <ComparrotContainer width={800} height={400}>
-      <ComparrotImage width={5} position={0} src={leftImage} alt="Left side of the comparison" />
-      <ComparrotImage width={95} position={1} src={rightImage} alt="Right side of the comparison" />
-    </ComparrotContainer>
+    <div>
+
+      <ComparrotContainer width={800} height={400}>
+        <ComparrotImage width={leftWidth} position={0} src={leftImage} alt="Left side of the comparison" />
+        <ComparrotImage width={rightWidth} position={1} src={rightImage} alt="Right side of the comparison" />
+      </ComparrotContainer>
+      <button onClick={() => adjustRight(20)}>Adjust</button>
+      <input type="range" min="0" max="100" value={leftWidth} onInput={(e) => adjustLeft(e.target.value)} />
+    </div>
   )
 }
