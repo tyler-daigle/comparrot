3482c6d77c8ce53f96c8ed9c36877a47b8a1729f
Tyler D <tyler@tdaigle.com>
2023-08-12 16:25:42 -0400

    working with giant progress bar

--- src/components/Comparrot.tsx
+++ src/components/Comparrot.tsx
@@ -1,13 +1,13 @@
 import ComparrotContainer from "./ComparrotContainer";
 import ComparrotImage from "./ComparrotImage";
+import styles from "../styles/Comparrot.module.css";
+
 import { useState } from "react";
 
 interface Props {
   leftImage: string;
   rightImage: string;
 };
-
-
 
 export default function Comparrot({ leftImage, rightImage }: Props) {
   const [leftWidth, setLeftWidth] = useState(50);
@@ -21,21 +21,14 @@ export default function Comparrot({ leftImage, rightImage }: Props) {
     setRightWidth(right);
   };
 
-  const adjustRight = (val: number) => {
-    const left = 100 - val;
-    setRightWidth(val);
-    setLeftWidth(left);
-  };
-
   return (
     <div>
 
       <ComparrotContainer width={800} height={400}>
         <ComparrotImage width={leftWidth} position={0} src={leftImage} alt="Left side of the comparison" />
         <ComparrotImage width={rightWidth} position={1} src={rightImage} alt="Right side of the comparison" />
+        <input className={styles.slider} type="range" min="0" max="100" value={leftWidth} onInput={(e) => adjustLeft(e.target.value)} />
       </ComparrotContainer>
-      <button onClick={() => adjustRight(20)}>Adjust</button>
-      <input type="range" min="0" max="100" value={leftWidth} onInput={(e) => adjustLeft(e.target.value)} />
     </div>
   )
 }
